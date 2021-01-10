8d8ac3eda965145ba1889eb1112c3916ccece7e6
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-01-10 01:33:36 +0100

    update for curren Ease\Html\Form

--- src/AbraFlexi/ui/TWB4/ConnectionForm.php
+++ src/AbraFlexi/ui/TWB4/ConnectionForm.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * AbraFlexi Bricks - Connection Config Form
  *
@@ -7,16 +8,17 @@
 
 namespace AbraFlexi\ui\TWB4;
 
-use \Ease\TWB4\Form;
-use \Ease\TWB4\SubmitButton;
+use Ease\TWB4\Form;
+use Ease\TWB4\SubmitButton;
+use Ease\Html\InputTextTag;
 
 /**
  * Form to configure used Abraflexi instance
  *
  * @author vitex
  */
-class ConnectionForm extends Form
-{
+class ConnectionForm extends Form {
+
     /**
      * Abraflexi URL Input name
      * @var string eg. https://demo.abraflexi.eu:5434
@@ -44,36 +46,33 @@ class ConnectionForm extends Form
     /**
      * Abraflexi Server connection form
      * 
-     * @param string $formAction    where to put response
-     * @param string $formMethod    GET | POST
-     * @param array  $tagProperties additional form tag propeties
+     * @param array $formProperties    FormTag properties eg. ['enctype' => 'multipart/form-data']
+     * @param array $formDivProperties FormDiv propertise eg. ['class'=>'form-row align-items-center']
+     * @param mixed $formContents      Any other initial content
      */
-    public function __construct($formAction = null, $formMethod = 'post',
-                                $tagProperties = null)
-    {
-        parent::__construct('SignIn', $formAction, $formMethod, null,
-            $tagProperties);
+    public function __construct($formProperties = [], $formDivProperties = [], $formContents = null) {
+        parent::__construct($formProperties, $formDivProperties, $formContents);
 
-        $this->addInput(new \Ease\Html\InputTextTag($this->urlField),
-            _('RestAPI endpoint url'));
+        $this->addInput(new InputTextTag($this->urlField),
+                _('RestAPI endpoint url'));
 
-        $this->addInput(new \Ease\Html\InputTextTag($this->usernameField),
-            _('REST API Username'));
+        $this->addInput(new InputTextTag($this->usernameField),
+                _('REST API Username'));
 
-        $this->addInput(new \Ease\Html\InputTextTag($this->passwordField),
-            _('Rest API Password'));
+        $this->addInput(new InputTextTag($this->passwordField),
+                _('Rest API Password'));
 
-        $this->addInput(new \Ease\Html\InputTextTag($this->companyField),
-            _('Company Code'));
+        $this->addInput(new InputTextTag($this->companyField),
+                _('Company Code'));
     }
 
     /**
      * Finally add subnut button
      */
-    public function finalize()
-    {
+    public function finalize() {
         $this->addItem(new SubmitButton(_('Use Connection'),
-                'success', ['width' => '100%']));
+                        'success', ['width' => '100%']));
         parent::finalize();
     }
+
 }
