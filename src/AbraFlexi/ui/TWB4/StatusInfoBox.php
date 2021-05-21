fda498544a3e444968d3f7fddcebaa18e9dfada9
Vítězslav Dvořák <info@vitexsoftware.cz>
2021-05-21 09:55:38 +0200

    Code cleanup

--- src/AbraFlexi/ui/TWB4/StatusInfoBox.php
+++ src/AbraFlexi/ui/TWB4/StatusInfoBox.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * AbraFlexi Bricks
  *
@@ -10,10 +11,10 @@ namespace AbraFlexi\ui\TWB4;
 /**
  * Abraflexi connection status widget
  */
-class StatusInfoBox extends \AbraFlexi\Company implements \Ease\Embedable
-{
+class StatusInfoBox extends \AbraFlexi\Company implements \Ease\Embedable {
 
     use \Ease\Glue;
+
     /**
      * Abraflexi Status
      * @var array
@@ -26,8 +27,7 @@ class StatusInfoBox extends \AbraFlexi\Company implements \Ease\Embedable
      * @param string|array $init    company dbNazev or initial data
      * @param array        $options Connection settings override
      */
-    public function __construct($init = null, $properites = [])
-    {
+    public function __construct($init = null, $properites = []) {
         parent::__construct($init, $properites);
         $infoRaw = $this->getFlexiData();
         if (count($infoRaw) && !array_key_exists('success', $infoRaw)) {
@@ -40,25 +40,24 @@ class StatusInfoBox extends \AbraFlexi\Company implements \Ease\Embedable
      * 
      * @return boolean
      */
-    public function connected()
-    {
+    public function connected() {
         return array_key_exists($this->getCompany(), $this->info);
     }
 
     /**
      * Draw result
      */
-    public function draw()
-    {
+    public function draw() {
         if ($this->connected()) {
             $myCompany = $this->getCompany();
-            $return    = new \Ease\TWB4\LinkButton($this->url.'/c/'.$myCompany,
-                $this->info[$myCompany]['nazev'], 'success');
+            $return = new \Ease\TWB4\LinkButton($this->url . '/c/' . $myCompany,
+                    $this->info[$myCompany]['nazev'], 'success');
         } else {
             $return = new \Ease\TWB4\LinkButton($this->getApiURL(),
-                _('Connection Problem'), 'danger');
+                    _('Connection Problem'), 'danger');
         }
 
         $return->draw();
     }
+
 }
